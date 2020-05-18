fab1e245b5553a24f44852410440c216b1df3cab
Rayleigh <rayleigh.2666@gmail.com>
2020-05-18 19:16:28 -0400

    MongoDB enabled

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 
+require('./models/User');
+require('./services/passport');
+
 const app = express();
+
+mongoose.connect(keys.mongoURI);
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,18 +1,35 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+	// user.id here is _id not googleID
+});
 
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
+			callbackURL: "/auth/google/callback",
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token\n', accessToken);
-			console.log('refresh token\n', refreshToken);
-			console.log('profile\n', profile);
+			// console.log('access token\n', accessToken);
+			// console.log('refresh token\n', refreshToken);
+			// console.log('profile\n', profile);
+			User.findOne({ googleID: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					// already exists, do nothing
+					done(null, existingUser);
+				} else {
+					new User({ googleID: profile.id })
+						.save()
+						.then((user) => done(null, user));
+				}
+			});
 		}
 	)
 );
