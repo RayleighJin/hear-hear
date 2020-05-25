5ae093844e9076aeba3f19107e80fefac453dc23
Rayleigh <rayleigh.2666@gmail.com>
2020-05-25 02:58:04 -0400

    Stripe enabled

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -9,7 +9,6 @@ import Header from "./Header";
 import Landing from "./Landing";
 const Dashbord = () => <h2>Dashbord</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-// const Landing = () => <h2>Landin</h2>;
 
 class App extends Component {
 	componentDidMount() {
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
 	renderContent() {
@@ -15,11 +16,14 @@ class Header extends Component {
 				);
 			default:
 				// logged in
-				return (
-					<li>
+				return [
+					<li key="1">
+						<Payments />
+					</li>,
+					<li key="2">
 						<a href="/api/logout">Logout</a>
-					</li>
-				);
+					</li>,
+				];
 		}
 	}
 
@@ -27,8 +31,11 @@ class Header extends Component {
 		console.log(this.props);
 		return (
 			<nav>
-				<div className="nav-wrapper">
-					<Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
+				<div className="nav-wrapper grey">
+					<Link
+						to={this.props.auth ? "/surveys" : "/"}
+						className="left brand-logo"
+					>
 						&emsp;hear-hear
 					</Link>
 					<ul className="right">{this.renderContent()}</ul>
